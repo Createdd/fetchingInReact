9dd121e92c61b38ac19faa0f6911c25e81948dde
Daniel Deutsch <deudan1010@gmail.com>
2017-06-16 16:12:22 +0200

    update

--- src/App.js
+++ src/App.js
@@ -4,24 +4,23 @@ import SearchForm from './Components/SearchForm';
 import ImgList from './Components/ImgList';
 
 export default class App extends Component {
-  
-  constructor() {
-    super();
-  } 
+	constructor() {
+		super();
+	}
 
-  render() { 
-    return (
-      <div>
-        <div className="main-header">
-          <div className="inner">
-            <h1 className="main-title">ImageSearch</h1>
-            <SearchForm />      
-          </div>   
-        </div>    
-        <div className="main-content">
-          <ImgList />
-        </div>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<div className="main-header">
+					<div className="inner">
+						<h1 className="main-title">ImageSearch</h1>
+						<SearchForm />
+					</div>
+				</div>
+				<div className="main-content">
+					<ImgList />
+				</div>
+			</div>
+		);
+	}
 }
--- src/Components/NoImgs.js
+++ src/Components/NoImgs.js
@@ -3,7 +3,7 @@ import React from 'react';
 const NoImgs = props => (
   <li className='no-imgs'>
     <i className="material-icons icon-img">sentiment_very_dissatisfied</i>
-    <h3>Sorry, no GIFs match your search.</h3>
+    <h3>Sorry, no Images match your search.</h3>
   </li>
 );
 
