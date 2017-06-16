b94639fc791550b75cc7a4682907281079e450be
Daniel Deutsch <deudan1010@gmail.com>
2017-06-16 18:21:33 +0200

    update basic fetch

--- src/App.js
+++ src/App.js
@@ -1,20 +1,63 @@
 import React, { Component } from 'react';
 import './App.css';
-import SearchForm from './Components/SearchForm';
+// import SearchForm from './Components/SearchForm';
 import ImgList from './Components/ImgList';
+
+import Unsplash from 'unsplash-js';
+
+// const unsplash = new Unsplash({
+// 	applicationId: process.env.APP_ID,
+// 	secret: process.env.APP_SECRET,
+// 	callbackUrl: process.env.CALLBACK_URL
+// });
+
+// const authenticationUrl = unsplash.auth.getAuthenticationUrl([
+//   "public"
+// ]);
+
+// location.assign(authenticationUrl);
+
+// unsplash.auth.userAuthentication(query.code)
+//   .then(toJson)
+//   .then(json => {
+//     unsplash.auth.setBearerToken(json.access_token);
+//   });
 
 export default class App extends Component {
 	constructor() {
 		super();
+		this.state = {
+			imgs: []
+		};
+	}
+
+	componentDidMount() {
+		// unsplash.photos
+		// 	.listPhotos(2, 15, 'latest')
+		// 	.then(toJson => toJson.json())
+		// 	.then(json => {
+		// 		this.setState({ imgs: json.data });
+		// 	})
+		// 	.catch(err => {
+		// 		console.log('Error happened during fetching!', err);
+		// 	});
+		fetch('https://api.unsplash.com/photos/?client_id=d3cfd3b8997abcd4c3fbe668a6c654a71412bcc7512710f116a76e7b095bfad8')
+			.then(toJson => toJson.json())
+			.then(json => {
+				this.setState({ imgs: json });
+			})
+			.catch(err => {
+				console.log('Error happened during fetching!', err);
+			});
 	}
 
 	render() {
+		console.log('state:' + this.state.imgs);
 		return (
 			<div>
 				<div className="main-header">
 					<div className="inner">
 						<h1 className="main-title">ImageSearch</h1>
-						<SearchForm />
 					</div>
 				</div>
 				<div className="main-content">
--- src/Components/SearchForm.js
+++ src/Components/SearchForm.js
@@ -1,30 +1,33 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
 
-export default class SearchForm extends Component {
-  
-  state = {
-    searchText: ''
-  }
-  
-  onSearchChange = e => {
-    this.setState({ searchText: e.target.value });
-  }
-  
-  handleSubmit = e => {
-    e.preventDefault();
-    e.currentTarget.reset();
-  }
-  
-  render() {  
-    return (
-      <form className="search-form" onSubmit={this.handleSubmit} >
-        <label className="is-hidden" htmlFor="search">Search</label>
-        <input type="search" 
-               onChange={this.onSearchChange}
-               name="search" 
-               placeholder="Search..." />
-        <button type="submit" id="submit" className="search-button"><i className="material-icons icn-search">search</i></button>
-      </form>      
-    );
-  }
-}
+// export default class SearchForm extends Component {
+// 	state = {
+// 		searchText: ''
+// 	};
+
+// 	onSearchChange = e => {
+// 		this.setState({ searchText: e.target.value });
+// 	};
+
+// 	handleSubmit = e => {
+// 		e.preventDefault();
+// 		e.currentTarget.reset();
+// 	};
+
+// 	render() {
+// 		return (
+// 			<form className="search-form" onSubmit={this.handleSubmit}>
+// 				<label className="is-hidden" htmlFor="search">Search</label>
+// 				<input
+// 					type="search"
+// 					onChange={this.onSearchChange}
+// 					name="search"
+// 					placeholder="Search..."
+// 				/>
+// 				<button type="submit" id="submit" className="search-button">
+// 					<i className="material-icons icn-search">search</i>
+// 				</button>
+// 			</form>
+// 		);
+// 	}
+// }
