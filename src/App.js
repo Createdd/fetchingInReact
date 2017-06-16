50f8a3fdd9b8a1931cb952b2a6a5e0666e03f9f6
Daniel Deutsch <deudan1010@gmail.com>
2017-06-16 23:00:21 +0200

    update basic fetch

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-// import SearchForm from './Components/SearchForm';
+import SearchForm from './Components/SearchForm';
 import ImgList from './Components/ImgList';
 
 import Unsplash from 'unsplash-js';
@@ -41,7 +41,9 @@ export default class App extends Component {
 		// 	.catch(err => {
 		// 		console.log('Error happened during fetching!', err);
 		// 	});
-		fetch('https://api.unsplash.com/photos/?client_id=d3cfd3b8997abcd4c3fbe668a6c654a71412bcc7512710f116a76e7b095bfad8')
+		fetch(
+			'https://api.unsplash.com/photos/?client_id=d3cfd3b8997abcd4c3fbe668a6c654a71412bcc7512710f116a76e7b095bfad8'
+		)
 			.then(toJson => toJson.json())
 			.then(json => {
 				this.setState({ imgs: json });
@@ -58,6 +60,7 @@ export default class App extends Component {
 				<div className="main-header">
 					<div className="inner">
 						<h1 className="main-title">ImageSearch</h1>
+						<SearchForm />
 					</div>
 				</div>
 				<div className="main-content">
