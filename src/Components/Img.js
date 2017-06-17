91004089540ccd2dd6265913f6f4b981dbb11582
Daniel Deutsch <deudan1010@gmail.com>
2017-06-17 14:24:31 +0200

    add credits for guidelines

--- src/Components/Img.js
+++ src/Components/Img.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
-const Img = props => (
-  <li className="img-wrap">
-    <img src={props.url} alt=""/>
-  </li>
-);
+const Img = props =>
+	<li>
+		<a href={props.link}>
+			<img src={props.url} alt="Unsplash Image here" />
+		</a>
+		<p>
+			Photo by
+			<a href={props.user}>{props.name}</a>
+			<a href={props.link}> See on Unsplash</a>
+		</p>
+	</li>;
 
 export default Img;
--- src/Components/ImgList.js
+++ src/Components/ImgList.js
@@ -6,7 +6,15 @@ const ImgList = props => {
 	const results = props.data;
 	let imgs;
 	if (results.length > 0) {
-		imgs = results.map(img => <Img url={img.urls.small} key={img.id} />);
+		imgs = results.map(img =>
+			<Img
+				url={img.urls.thumb}
+				user={img.user.links.html}
+				name={img.user.name}
+				link={img.links.html}
+				key={img.id}
+			/>
+		);
 	} else {
 		imgs = <NoImgs />;
 	}
