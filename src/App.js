45af0dbe38322b45391db50bef009d48d03b02f9
Daniel Deutsch <deudan1010@gmail.com>
2017-06-17 11:41:41 +0200

    update basic fetch

--- src/App.js
+++ src/App.js
@@ -1,30 +1,8 @@
-// import 'dotenv/config';
 import React, { Component } from 'react';
 import './App.css';
-import SearchForm from './Components/SearchForm';
 import ImgList from './Components/ImgList';
 
 import cred from './cred.js';
-
-// import Unsplash from 'unsplash-js';
-
-// const unsplash = new Unsplash({
-// 	applicationId: process.env.APP_ID,
-// 	secret: process.env.APP_SECRET,
-// 	callbackUrl: process.env.CALLBACK_URL
-// });
-
-// const authenticationUrl = unsplash.auth.getAuthenticationUrl([
-//   "public"
-// ]);
-
-// location.assign(authenticationUrl);
-
-// unsplash.auth.userAuthentication(query.code)
-//   .then(toJson)
-//   .then(json => {
-//     unsplash.auth.setBearerToken(json.access_token);
-//   });
 
 export default class App extends Component {
 	constructor() {
@@ -35,15 +13,6 @@ export default class App extends Component {
 	}
 
 	componentDidMount() {
-		// unsplash.photos
-		// 	.listPhotos(2, 15, 'latest')
-		// 	.then(toJson => toJson.json())
-		// 	.then(json => {
-		// 		this.setState({ imgs: json.data });
-		// 	})
-		// 	.catch(err => {
-		// 		console.log('Error happened during fetching!', err);
-		// 	});
 		fetch('https://api.unsplash.com/photos/?client_id=' + cred.APP_ID)
 			.then(res => res.json())
 			.then(data => {
@@ -60,7 +29,6 @@ export default class App extends Component {
 				<div className="main-header">
 					<div className="inner">
 						<h1 className="main-title">ImageSearch</h1>
-						<SearchForm />
 					</div>
 				</div>
 				<div className="main-content">
